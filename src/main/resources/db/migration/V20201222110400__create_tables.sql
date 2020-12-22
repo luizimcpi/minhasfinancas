2a9537422fa12adc847c2474077e40e4ad36ffbe
Luiz Evangelista <luiz.evangelista@meupag.com.br>
2020-12-22 11:26:13 -0300

    alterando script de criação de tabelas

--- src/main/resources/db/migration/V20201222110400__create_tables.sql
+++ src/main/resources/db/migration/V20201222110400__create_tables.sql
@@ -4,7 +4,7 @@ CREATE TABLE public.usuario
   nome character varying(150),
   email character varying(100),
   senha character varying(20),
-  data_cadastro date default now()
+  data_cadastro date DEFAULT now()
 );
 
 CREATE TABLE public.lancamento
@@ -13,9 +13,9 @@ CREATE TABLE public.lancamento
   descricao character varying(100) NOT NULL,
   mes integer NOT NULL,
   ano integer NOT NULL,
-  valor numeric(16,2),
-  tipo character varying(20),
-  status character varying(20),
-  id_usuario bigint REFERENCES public.usuario (id),
-  data_cadastro date default now()
+  valor numeric(16,2) NOT NULL,
+  tipo character varying(20) CHECK ( tipo in ('RECEITA', 'DESPESA') ) NOT NULL,
+  status character varying(20) CHECK ( status IN ('PENDENTE', 'CANCELADO', 'EFETIVADO') ) NOT NULL,
+  id_usuario bigint REFERENCES public.usuario (id) NOT NULL,
+  data_cadastro date DEFAULT now()
 );
