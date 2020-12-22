f98d83944c7064ca8e27e06679034010aadb9437
Luiz Evangelista <luiz.evangelista@meupag.com.br>
2020-12-22 11:19:43 -0300

    adicionando estrutura inicial

---
+++ src/main/resources/db/migration/V20201222110400__create_tables.sql
@@ -0,0 +1,21 @@
+CREATE TABLE public.usuario
+(
+  id bigserial NOT NULL PRIMARY KEY,
+  nome character varying(150),
+  email character varying(100),
+  senha character varying(20),
+  data_cadastro date default now()
+);
+
+CREATE TABLE public.lancamento
+(
+  id bigserial NOT NULL PRIMARY KEY ,
+  descricao character varying(100) NOT NULL,
+  mes integer NOT NULL,
+  ano integer NOT NULL,
+  valor numeric(16,2),
+  tipo character varying(20),
+  status character varying(20),
+  id_usuario bigint REFERENCES public.usuario (id),
+  data_cadastro date default now()
+);
