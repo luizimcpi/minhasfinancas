5b707a6f32b33e320abd03c01c53fa85f19ac057
Luiz Evangelista <luiz.evangelista@meupag.com.br>
2020-12-22 11:47:37 -0300

    adicionando classes de entity do banco

---
+++ src/main/java/com/devlhse/minhasfinancas/model/entity/StatusLancamento.java
@@ -0,0 +1,8 @@
+package com.devlhse.minhasfinancas.model.entity;
+
+public enum StatusLancamento {
+
+	PENDENTE,
+	CANCELADO,
+	EFETIVADO
+}
---
+++ src/main/java/com/devlhse/minhasfinancas/model/entity/TipoLancamento.java
@@ -0,0 +1,7 @@
+package com.devlhse.minhasfinancas.model.entity;
+
+public enum TipoLancamento {
+
+	RECEITA,
+	DESPESA
+}
