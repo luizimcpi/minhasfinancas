2cfc9fe6b942468efedcd86055c2e31eb5e98514
Luiz Evangelista <luiz.evangelista@meupag.com.br>
2020-12-22 12:03:25 -0300

    adicionando lombok e excluindo codigo desnecessario das entities

--- src/main/java/com/devlhse/minhasfinancas/model/entity/Usuario.java
+++ src/main/java/com/devlhse/minhasfinancas/model/entity/Usuario.java
@@ -1,15 +1,19 @@
 package com.devlhse.minhasfinancas.model.entity;
 
+import lombok.Builder;
+import lombok.Data;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import java.util.Objects;
 
 @Entity
 @Table( name = "usuario", schema = "public")
+@Builder
+@Data
 public class Usuario {
 
 	@Id
@@ -26,53 +30,4 @@ public class Usuario {
 	@Column( name = "senha")
 	private String senha;
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getSenha() {
-		return senha;
-	}
-
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-
-	@Override public boolean equals(Object o) {
-		if (this == o)
-			return true;
-		if (o == null || getClass() != o.getClass())
-			return false;
-		Usuario usuario = (Usuario) o;
-		return id.equals(usuario.id) && nome.equals(usuario.nome) && email.equals(usuario.email) && senha.equals(
-				usuario.senha);
-	}
-
-	@Override public int hashCode() {
-		return Objects.hash(id, nome, email, senha);
-	}
-
-	@Override public String toString() {
-		return "Usuario{" + "id=" + id + ", nome='" + nome + '\'' + ", email='" + email + '\'' + ", senha='" + senha + '\'' + '}';
-	}
 }
