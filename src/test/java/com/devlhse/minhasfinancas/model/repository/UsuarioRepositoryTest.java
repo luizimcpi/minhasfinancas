07291a8005079a4f2edb428864fd06a9815b1905
Luiz Evangelista <luizhenrique.se@gmail.com>
2020-12-24 13:22:55 -0800

    voltando configuração de testes

--- src/test/java/com/devlhse/minhasfinancas/model/repository/UsuarioRepositoryTest.java
+++ src/test/java/com/devlhse/minhasfinancas/model/repository/UsuarioRepositoryTest.java
@@ -7,15 +7,13 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@SpringBootTest
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
-@DataJpaTest
-@AutoConfigureTestDatabase
 public class UsuarioRepositoryTest extends AbstractIntegrationTest {
 
 	@Autowired
