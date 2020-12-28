44337f98134887a144ddbd0054f6be37714010e8
Luiz Evangelista <luizhenrique.se@gmail.com>
2020-12-28 12:30:04 -0300

    adicionando cenarios de teste do lancamentoService

--- src/test/java/com/devlhse/minhasfinancas/service/LancamentoServiceTest.java
+++ src/test/java/com/devlhse/minhasfinancas/service/LancamentoServiceTest.java
@@ -8,14 +8,19 @@ import com.devlhse.minhasfinancas.model.enums.TipoLancamento;
 import com.devlhse.minhasfinancas.model.repository.LancamentoRepository;
 import com.devlhse.minhasfinancas.service.impl.LancamentoServiceImpl;
 import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Example;
 
 @ExtendWith(MockitoExtension.class)
 public class LancamentoServiceTest {
@@ -29,6 +34,7 @@ public class LancamentoServiceTest {
 	@Test
 	public void deveSalvarUmLancamento(){
 		Lancamento lancamentoASalvar = criarLancamentoValido();
+		lancamentoASalvar.setId(1l);
 		service.salvar(lancamentoASalvar);
 		verify(repository, times(1)).save(lancamentoASalvar);
 	}
@@ -54,36 +60,90 @@ public class LancamentoServiceTest {
 	@Test
 	public void deveAtualizarUmLancamento(){
 		Lancamento lancamentoAAtualizar = criarLancamentoValido();
+		lancamentoAAtualizar.setId(1l);
 		service.atualizar(lancamentoAAtualizar);
 		verify(repository, times(1)).save(lancamentoAAtualizar);
 	}
 
 	@Test
 	public void naoDeveAtualizarUmLancamentoSemId(){
-		Lancamento lancamentoAAtualizar = criarLancamentoSemId();
+		Lancamento lancamentoAAtualizar = criarLancamentoValido();
 		Assertions.assertThrows(NullPointerException.class, () -> {
 			service.atualizar(lancamentoAAtualizar);
 		});
 		verify(repository, times(0)).save(lancamentoAAtualizar);
 	}
 
-	private Lancamento criarLancamentoValido(){
-
-		Usuario usuarioValido = criarUsuarioValido();
-
-		return Lancamento.builder()
-				.id(1l)
-				.ano(2020)
-				.mes(1)
-				.descricao("lançamento teste")
-				.valor(BigDecimal.TEN)
-				.tipo(TipoLancamento.DESPESA)
-				.usuario(usuarioValido)
-				.status(StatusLancamento.CANCELADO)
-				.build();
+	@Test
+	public void deveDeletarUmLancamento(){
+		Lancamento lancamento = criarLancamentoValido();
+		lancamento.setId(1l);
+		service.deletar(lancamento);
+		verify(repository, times(1)).delete(lancamento);
 	}
 
-	private Lancamento criarLancamentoSemId(){
+	@Test
+	public void naoDeveDeletarUmLancamentoSemId(){
+		Lancamento lancamento = criarLancamentoValido();
+		Assertions.assertThrows(NullPointerException.class, () -> {
+			service.deletar(lancamento);
+		});
+		verify(repository, times(0)).delete(lancamento);
+	}
+
+	@Test
+	public void deveFiltrarLancamentos(){
+		Lancamento lancamento = criarLancamentoValido();
+		lancamento.setId(1l);
+
+		List<Lancamento> lancamentos = Arrays.asList(lancamento);
+
+		when(repository.findAll(any(Example.class))).thenReturn(lancamentos);
+
+		List<Lancamento> resultado = service.buscar(lancamento);
+
+		Assertions.assertEquals(1, resultado.size());
+	}
+
+	@Test
+	public void deveAtualizarStatusLancamento(){
+		Lancamento lancamentoAAtualizar = criarLancamentoValido();
+		lancamentoAAtualizar.setId(1l);
+		service.atualizarStatus(lancamentoAAtualizar, StatusLancamento.PENDENTE);
+		verify(repository, times(1)).save(lancamentoAAtualizar);
+	}
+
+	@Test
+	public void naoDeveAtualizarStatusDeUmLancamentoSemId(){
+		Lancamento lancamentoAAtualizar = criarLancamentoValido();
+		Assertions.assertThrows(NullPointerException.class, () -> {
+			service.atualizarStatus(lancamentoAAtualizar, StatusLancamento.PENDENTE);
+		});
+		verify(repository, times(0)).save(lancamentoAAtualizar);
+	}
+
+	@Test
+	public void deveObterLancamentoPorId(){
+		Lancamento lancamento = criarLancamentoValido();
+		lancamento.setId(1l);
+		when(repository.findById(anyLong())).thenReturn(Optional.of(lancamento));
+		Optional<Lancamento> lancamentoEncontrado = service.obterPorId(1l);
+		assertTrue(lancamentoEncontrado.isPresent());
+		assertEquals(1l, lancamentoEncontrado.get().getId());
+		assertEquals("lançamento teste", lancamentoEncontrado.get().getDescricao());
+		assertEquals(2020, lancamentoEncontrado.get().getAno());
+		assertEquals(1, lancamentoEncontrado.get().getMes());
+	}
+
+	@Test
+	public void naoDeveObterLancamentoQuandoIdNaoExistir(){
+		when(repository.findById(anyLong())).thenReturn(Optional.empty());
+		Optional<Lancamento> lancamentoEncontrado = service.obterPorId(1l);
+		assertTrue(lancamentoEncontrado.isEmpty());
+	}
+
+
+	private Lancamento criarLancamentoValido(){
 
 		Usuario usuarioValido = criarUsuarioValido();
 
