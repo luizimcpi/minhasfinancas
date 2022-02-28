a09ae7001060951182fbf7c4adabcfcdac622ddc
Luiz Henrique <luizhenrique.se@gmail.com>
2022-02-28 00:24:56 -0300

    feat: criand controle pin

---
+++ src/main/java/com/devlhse/minhasfinancas/model/repository/ControlePinRepository.java
@@ -0,0 +1,11 @@
+package com.devlhse.minhasfinancas.model.repository;
+
+import com.devlhse.minhasfinancas.model.entity.ControlePin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ControlePinRepository extends JpaRepository<ControlePin, Long> {
+
+    Optional<ControlePin> findByEmail(String email);
+}
--- src/main/java/com/devlhse/minhasfinancas/service/impl/UserDetailsServiceImpl.java
+++ src/main/java/com/devlhse/minhasfinancas/service/impl/UserDetailsServiceImpl.java
@@ -3,6 +3,7 @@ package com.devlhse.minhasfinancas.service.impl;
 import com.devlhse.minhasfinancas.model.entity.CustomUserDetails;
 import com.devlhse.minhasfinancas.model.entity.Usuario;
 import com.devlhse.minhasfinancas.model.repository.UsuarioRepository;
+import com.devlhse.minhasfinancas.service.CriadorControlePin;
 import com.devlhse.minhasfinancas.service.EmailService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,6 +25,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     private UsuarioRepository repository;
     private EmailService emailService;
+    private CriadorControlePin criadorControlePin;
 
     @Override
     public UserDetails loadUserByUsername(String email) {
@@ -31,11 +33,13 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
         if (usuario.isPresent()){
             if(!usuario.get().isAtivo()) {
-                final var randomNumber = getSixDigitsRandomNumberString();
-                emailService.enviarEmail(usuario.get().getEmail(),
-                        "Ativação de usuário - Minhas Finanças",
-                        "Informe o seguinte código de segurança na página de login para ativar seu usuário: <b>" + randomNumber + "</b>");
+                final var pin = getSixDigitsRandomNumberString();
                 log.warn("Usuario id: {} está inativo publicando evento de envio email.", usuario.get().getId());
+                emailService.enviarEmail(email,
+                        "Ativação de usuário - Minhas Finanças",
+                        "Informe o seguinte código de segurança na página de login para ativar seu usuário: <b>" + pin + "</b>. Este código expira em 1 hora.");
+                log.warn("Usuario id: {} está inativo registrando controle pin.", usuario.get().getId());
+                criadorControlePin.criaControlePin(email, pin);
                 throw new InsufficientAuthenticationException("Usuario Inativo, favor ativar no link enviado para o email");
             }
             var userDetails = new CustomUserDetails(usuario.get(),
