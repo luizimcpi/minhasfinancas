cb72b5cae96ab6478b949d86cc03a1e99c97c74b
Luiz Henrique <luiz.henrique@idwall.co>
2022-07-15 09:48:27 -0300

    fix: migration with new fields in table user

--- src/main/resources/db/migration/V20220714191300__create_fields_valido_table_usuario.sql
+++ src/main/resources/db/migration/V20220714191300__create_fields_valido_table_usuario.sql
@@ -1,3 +1,3 @@
 ALTER TABLE usuario ADD COLUMN "valido" BOOLEAN NOT NULL DEFAULT FALSE;
 
-ALTER TABLE usuario ADD COLUMN "data_alteracao" timestamp without time zone NOT NULL;
+ALTER TABLE usuario ADD COLUMN "data_alteracao" timestamp without time zone NOT NULL DEFAULT now();
